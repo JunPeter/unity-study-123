830ab14d778d23826c9cf72da71cead61b36cc73
JunPeter <peter950306@gmail.com>
2025-05-22 02:47:04 +0900

    유니티 코드 만들었다
    만들었다고

---
+++ Assets/Scripts/uniry code.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class unirycode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
